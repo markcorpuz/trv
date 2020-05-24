460ec8f520c5a7d0583d7d4af46e4124dad9958a
Mark Corpuz <social@markcorpuz.com>
2020-05-23 17:36:36 -0700

    SETUP-BE | Implementing multiple version of author display on archive

--- partials/archive.php
+++ partials/archive.php
@@ -10,17 +10,18 @@
 
 echo '<article class="module post-summary">';
 
-	setup_be_post_summary_image();
+	setup_be_image_post_summary();
 
 	echo '<div class="items info post-summary__content">';
 		setup_be_overline();
-		setup_be_post_summary_title();
+		setup_be_title_post_summary();
+		setup_be_author_gravatar_name();
 		//ea_entry_author();
 		//setup_be_author_name();
 		//setup_be_date_published();
-		setup_be_date_published_author();
+		//setup_be_date_published_author();
 		//setup_be_excerpt();
-		setup_be_excerpt_only();
+		//setup_be_excerpt_only();
 	echo '</div>';
 
 echo '</article>';
