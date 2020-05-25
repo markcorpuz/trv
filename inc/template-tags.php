54e79f7aab1f2336f9fd5bbb4959ee6ef12049c8
Mark Corpuz <social@markcorpuz.com>
2020-05-25 07:57:24 -0700

    SETUP-BE | Overhauled the archive partials with module item(s) structure

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -8,71 +8,96 @@
  * @license      GPL-2.0+
 **/
 
+
 /**
- * Entry Category
+ * IMPORTANT NOTES
+ *
+ * Bill's code is retained because a lot of his functions are being used on other layouts (like regular posts).
+ * Since I can't determine where those dependencies are, I just leave them (prefixed by ea_).
+ * But moving forward, I'm using my own module/item(s) structure & logic.
+ */
+
+
+/**
+ * FEATURED IMAGE
+ * 
+ */
+
+function ea_post_summary_image( $size = 'thumbnail_medium' ) {
+	echo '<a class="post-summary__image" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . wp_get_attachment_image( ea_entry_image_id(), $size ) . '</a>';
+}
+
+function setup_be_image( $size = 'thumbnail_medium' ) {
+	echo '<a class="item image link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . wp_get_attachment_image( ea_entry_image_id(), $size ) . '</a>';
+}
+function setup_be_image_nolink( $size = 'thumbnail_medium' ) {
+	echo wp_get_attachment_image( ea_entry_image_id(), $size );
+}
+
+/**
+ * Entry Image ID
  *
  */
+
+function ea_entry_image_id() {
+	return has_post_thumbnail() ? get_post_thumbnail_id() : get_option( 'options_ea_default_image' );
+}
+
+
+/**
+ * OVERLINE
+ * 
+ */
+
 function ea_entry_category() {
 	$term = ea_first_term();
 	if( !empty( $term ) && ! is_wp_error( $term ) )
 		echo '<p class="entry-category"><a href="' . get_term_link( $term, 'category' ) . '">' . $term->name . '</a></p>';
 }
 
-/**
- * Overline
- *
- */
 function setup_be_overline() {
 	$term = ea_first_term();
 	if( !empty( $term ) && ! is_wp_error( $term ) )
-		echo '<p class="item overline"><a href="' . get_term_link( $term, 'category' ) . '">' . $term->name . '</a></p>';
+		echo '<div class="item overline"><a href="' . get_term_link( $term, 'category' ) . '">' . $term->name . '</a></div>';
+}
+function setup_be_overline_nolink() {
+	$term = ea_first_term();
+	if( !empty( $term ) && ! is_wp_error( $term ) )
+		echo '<div class="item overline">' . $term->name . '</div>';
 }
 
+
 /**
- * Post Summary Title
- *
+ * TITLE
+ * 
  */
+
 function ea_post_summary_title() {
 	global $wp_query;
 	$tag = ( is_singular() || -1 === $wp_query->current_post ) ? 'h3' : 'h2';
 	echo '<' . $tag . ' class="post-summary__title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></' . $tag . '>';
 }
-function setup_be_title_post_summary() {
+
+function setup_be_title() {
 	global $wp_query;
 	$tag = ( is_singular() || -1 === $wp_query->current_post ) ? 'h3' : 'h2';
-	echo '<' . $tag . ' class="item title post-summary__title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></' . $tag . '>';
+	echo '<' . $tag . ' class="item title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></' . $tag . '>';
+}
+function setup_be_title_nolink() {
+	global $wp_query;
+	$tag = ( is_singular() || -1 === $wp_query->current_post ) ? 'h3' : 'h2';
+	echo '<' . $tag . ' class="item title">' . get_the_title() . '</' . $tag . '>';
 }
 
 /**
- * Post Summary Image
+ * AUTHOR
  *
  */
-function ea_post_summary_image( $size = 'thumbnail_medium' ) {
-	echo '<a class="post-summary__image" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . wp_get_attachment_image( ea_entry_image_id(), $size ) . '</a>';
-}
-function setup_be_image_post_summary( $size = 'thumbnail_medium' ) {
-	echo '<a class="item image post-summary__image" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . wp_get_attachment_image( ea_entry_image_id(), $size ) . '</a>';
-}
 
-
-/**
- * Entry Image ID
- *
- */
-function ea_entry_image_id() {
-	return has_post_thumbnail() ? get_post_thumbnail_id() : get_option( 'options_ea_default_image' );
-}
-
-/**
- * Entry Author
- *
- */
 function ea_entry_author() {
 	$id = get_the_author_meta( 'ID' );
-	echo '<p class="entry-author"><a href="' . get_author_posts_url( $id ) . '" aria-hidden="true" tabindex="-1">' . get_avatar( $id, 40 ) . '</a><em>by</em> <a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></p>';
+	echo '<div class="item author">By <a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></div>';
 }
-
-// AUTHOR
 
 	/**
 	 * AUTHOR (name only)
@@ -82,6 +107,10 @@ function ea_entry_author() {
 		$id = get_the_author_meta( 'ID' );
 		echo '<div class="item author"><a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></div>';
 	}
+	function setup_be_author_by_name() {
+		$id = get_the_author_meta( 'ID' );
+		echo '<div class="item author">By <a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></div>';
+	}
 
 	/**
 	 * AUTHOR ICON (icon w name)
@@ -89,11 +118,15 @@ function ea_entry_author() {
 	 */
 	function setup_be_author_gravatar_name() {
 		$id = get_the_author_meta( 'ID' );
+		echo '<div class="item author icon"><a class="item author gravatar" href="' . get_author_posts_url( $id ) . '" aria-hidden="true" tabindex="-1">' . get_avatar( $id, 20 ) . '</a><span><a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></div>';
+	}
+	function setup_be_author_gravatar_by_name() {
+		$id = get_the_author_meta( 'ID' );
 		echo '<div class="item author icon"><a class="item author gravatar" href="' . get_author_posts_url( $id ) . '" aria-hidden="true" tabindex="-1">' . get_avatar( $id, 20 ) . '</a>by&nbsp;<span><a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></div>';
 	}
 
 	/**
-	 * AUTHOR ICONONLY (icon)
+	 * AUTHOR ICONONLY (icon only)
 	 *
 	 */
 	function setup_be_author_gravatar() {
@@ -101,45 +134,83 @@ function ea_entry_author() {
 		echo '<a class="item author gravatar" href="' . get_author_posts_url( $id ) . '" aria-hidden="true" tabindex="-1">' . get_avatar( $id, 20 ) . '</a>';
 	}
 
-/**
- * Date Published
- * 
- */
-function setup_be_date_published() {
-	echo '<p class="item date">' . get_the_date( 'M d Y' ) . '</p>';
-}
 
 /**
- * Date & Author
- * 
+ * DATE
+ *
  */
-function setup_be_date_published_author() {
-	$id = get_the_author_meta( 'ID' );
-	echo '<div class="items meta">';
-	echo '<span class="item date">' . get_the_date( 'M d Y' ) . ' </span>';
-	echo '<span class="item author">by <a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></span>';
-	echo '</div>';
-}
+
+	/**
+	 * DATE (Jan 3, 2020)
+	 *
+	 */
+	function setup_be_date_mdy() {
+		echo '<div class="item date">' . get_the_date( 'M j, Y' ) . '</div>';
+	}
+	/**
+	 * MONTH |  DATE (Oct | Jan 3, 2020)
+	 *
+	 */
+	function setup_be_date_weekday_mdy() {
+		echo '<div class="item date">' . get_the_date( 'D | M j, Y' ) . '</div>';
+	}
+
 
 /**
- * Excerpt or Max Words
+ * DATE & AUTHOR
  * 
  */
-function setup_be_excerpt() {
-    $max_words = 10;
-    $the_excerpt = get_the_excerpt();
-    if( $the_excerpt ) {
-        echo '<div class="item excerpt">'.wp_trim_words( $the_excerpt, $max_words ).'</div>';
-    }
-}
+
+	/**
+	 * DATE by AUTHOR (Jan 3, 2020 By Mark)
+	 *
+	 */
+	function setup_be_date_by_author_name() {
+		$id = get_the_author_meta( 'ID' );
+		echo '<div class="items date_by_author_name">';
+		echo '<span class="item date">' . get_the_date( 'M j, Y' ) . '</span>';
+		echo '<span class="item author">By <a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></span>';
+		echo '</div>';
+	}
+	/**
+	 * DATE vbar AUTHOR (Jan 3, 2020 | Mark)
+	 *
+	 */
+	function setup_be_date_vbar_author_name() {
+		$id = get_the_author_meta( 'ID' );
+		echo '<div class="items date_vbar_author_name">';
+		echo '<span class="item date">' . get_the_date( 'M j, Y' ) . '</span>';
+		echo '<span class="item vbar"> | </span>';
+		echo '<span class="item author"><a href="' . get_author_posts_url( $id ) . '">' . get_the_author() . '</a></span>';
+		echo '</div>';
+	}
+
+
 /**
- * Excerpt Only
- * 
+ * EXCERPT
+ *
  */
-function setup_be_excerpt_only() {
-    if( has_excerpt() ) {
-    	echo '<div class="item excerpt">' . get_the_excerpt() . '</div>';
-    } else {
-    	echo '';
-    }
-}
+
+	/**
+	 * EXCERPT ONLY
+	 * 
+	 */
+	function setup_be_excerpt() {
+	    if( has_excerpt() ) {
+	    	echo '<div class="item excerpt">' . get_the_excerpt() . '</div>';
+	    } else {
+	    	//echo '';
+	    }
+	}
+
+	/**
+	 * EXCERPT OR MAX WORDS
+	 * 
+	 */
+	function setup_be_excerpt_maxwords() {
+	    $max_words = 10;
+	    $the_excerpt = get_the_excerpt();
+	    if( $the_excerpt ) {
+	        echo '<div class="item excerpt">'.wp_trim_words( $the_excerpt, $max_words ).'</div>';
+	    }
+	}
--- partials/archive.php
+++ partials/archive.php
@@ -10,18 +10,38 @@
 
 echo '<article class="module post-summary">';
 
-	setup_be_image_post_summary();
+	// FEATURED IMAGE
+	setup_be_image();
+	//setup_be_image_nolink();
 
-	echo '<div class="items info post-summary__content">';
+	echo '<div class="items info">';
+
+		// OVERLINE
 		setup_be_overline();
-		setup_be_title_post_summary();
-		setup_be_author_gravatar_name();
-		//ea_entry_author();
+		//setup_be_overline_nolink();
+		
+		// TITLE
+		setup_be_title();
+		//setup_be_title_nolink();
+
+		// AUTHOR
 		//setup_be_author_name();
-		//setup_be_date_published();
-		//setup_be_date_published_author();
-		//setup_be_excerpt();
-		//setup_be_excerpt_only();
+		//setup_be_author_by_name();
+		//setup_be_author_gravatar_by_name();
+		//setup_be_author_gravatar();
+		
+		// DATE
+		//setup_be_date_mdy();
+		//setup_be_date_weekday_mdy();
+
+		// DATE & AUTHOR
+		setup_be_date_vbar_author_name();
+		//setup_be_date_by_author_name();
+
+		// EXCERPT
+		setup_be_excerpt();
+		//setup_be_excerpt_maxwords();
+
 	echo '</div>';
 
 echo '</article>';
